60569647fe59bc55b32f403b7d03a63ca450ea3e
vibhu1885 <vibhu1885@gmail.com>
2026-02-02 00:53:40 +0530

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -439,7 +439,7 @@ elif st.session_state.page == 'admin_dashboard':
                             st.success("Grievance Officer Successfully Assigned!")
                             time.sleep(0.5)
                             st.rerun()
-                        except: st.error("Refresh Page to See Current Status")
+                        except: st.error("Grievance status changed refresh page to see updated Status")
                 else:
                     assign_date = row.get('ASSIGN_DATE', row.get('OFFICER_REMARK', 'N/A')) 
                     st.markdown(f"""
@@ -537,7 +537,7 @@ elif st.session_state.page == 'officer_dashboard':
                                 st.success("Resolved Successfully!")
                                 time.sleep(0.5)
                                 st.rerun()
-                            except: st.error("Update Error")
+                            except: st.error("Grievance status changed refresh page to see updated Status")
                 else:
                     st.markdown(f"""
                     <div style="background-color: #2c2e3a; padding: 10px; border-radius: 8px;">
