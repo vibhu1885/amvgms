28e9b97ee7ac245c3f5887b0d459c276a26bca7e
vibhu1885 <vibhu1885@gmail.com>
2026-02-02 14:38:25 +0530

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -13,7 +13,7 @@ import gspread
 # ==========================================
 LOGO_PATH = "assets/office_logo.png"
 LOGO_WIDTH = 225
-APP_BG_COLOR = "#404154"
+APP_BG_COLOR = "#acadb0"
 
 # Initialize State
 if 'page' not in st.session_state: st.session_state.page = 'landing'
